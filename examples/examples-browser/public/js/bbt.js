01f2d5d097d84f43a3c1c9f30d68836e2f83869b
George Fridrich <30667989+georgefridrich@users.noreply.github.com>
2019-12-05 10:28:23 -0700

    Update bbt.js

--- examples/examples-browser/public/js/bbt.js
+++ examples/examples-browser/public/js/bbt.js
@@ -30,14 +30,14 @@ function renderFaceImageSelectList(selectListId, onChange, initialValue) {
 }
 
 // fetch first image of each class and compute their descriptors
-async function createBbtFaceMatcher(numImagesForTraining = 2) {
+async function createBbtFaceMatcher(numImagesForTraining = 1) {
   const maxAvailableImagesPerClass = 6
   numImagesForTraining = Math.min(numImagesForTraining, maxAvailableImagesPerClass)
 
   const labeledFaceDescriptors = await Promise.all(classes.map(
     async className => {
       const descriptors = []
-      for (let i = 2; i < (numImagesForTraining + 1); i++) {
+      for (let i = 1; i < (numImagesForTraining + 1); i++) {
         const img = await faceapi.fetchImage(getFaceImageUri(className, i))
         descriptors.push(await faceapi.computeFaceDescriptor(img))
       }
