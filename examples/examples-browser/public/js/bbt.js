7223389379ababc670f77f2de4d9ff4450218c27
George Fridrich <30667989+georgefridrich@users.noreply.github.com>
2019-12-05 03:53:07 -0700

    added new images

--- examples/examples-browser/public/js/bbt.js
+++ examples/examples-browser/public/js/bbt.js
@@ -1,4 +1,4 @@
-const classes = ['kenneth', 'cleo', 'joel', 'rash', 'michael_s', 'rosaldo', 'manz', 'stephan', 'sam', 'rj']
+const classes = ['kenneth', 'cleo', 'joel', 'rash', 'michael_s', 'rosaldo', 'manz', 'stephan', 'sam', 'rj', 'michael_a', 'george']
 
 function getFaceImageUri(className, idx) {
   return `${className}/${className}${idx}.png`
